37d6602d0e206dffad112b4fd6530deb4d9fb3af
Kévin Rocha <neonix75@gmail.com>
2020-08-31 19:54:41 +0200

    Basic files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Feed from './componantes/Feed.jsx'
+
 function App() {
   return (
     <div className="App">
       <h1>Social Media App</h1>
+      <Feed></Feed>
     </div>
   );
 }
---
+++ src/componantes/Feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/feed.css'
+
+function Feed() {
+    return (
+      <div className="App">
+        <p>Feed</p>
+        <p>Feed</p>
+        <p>Feed</p>
+        <p>Feed</p>
+        <p>Feed</p>
+        <p>Feed</p>
+        <p>Feed</p>
+      </div>
+    );
+  }
+  
+  export default Feed;
